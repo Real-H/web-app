fa0d13c7f5177c348301bde241c541d3e8ec867b
Hassaan Ahmad <19011519-034@uog.edu.pk>
2020-09-09 22:58:34 +0500

    2nd commit

--- src/main/java/com/carmanagement/secure/WebSecurityConfig.java
+++ src/main/java/com/carmanagement/secure/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/edit/**", "/delete/**").hasRole("ADMIN")
+		.antMatchers("/edit/*", "/delete/**").hasRole("ADMIN")
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
